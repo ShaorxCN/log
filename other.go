df65a011cbbf67fe338973eb4bbf099bf2a904cc
Arstd <shangxuejin@gmail.com>
2016-07-18 10:30:59 +0800

    LevelXxx => Lxxx

--- other.go
+++ other.go
@@ -9,16 +9,6 @@ import (
 )
 
 // ======== 兼容 qiniu/log   ===============
-const (
-	Ldebug int = int(LevelDebug)
-	Linfo      = int(LevelInfo)
-	Lwarn      = int(LevelWarn)
-	Lerror     = int(LevelError)
-	Lpanic     = int(LevelPanic)
-	Lfatal     = int(LevelFatal)
-)
-
-// ======== 兼容 qiniu/log   ===============
 func SetOutputLevel(l int) { v = Level(l) }
 
 // ======== 兼容 wothing/log ===============
@@ -35,7 +25,7 @@ func SetOutputLevel(l int) { v = Level(l) }
 // TraceIn 方法入口打印日志
 func TraceIn(tag string, method string, format string, m ...interface{}) (string, string, time.Time) {
 	startTime := time.Now()
-	std.Tprintf(LevelInfo, tag, "calling "+method+", "+format, m...)
+	std.Tprintf(Linfo, tag, "calling "+method+", "+format, m...)
 	return tag, method, startTime
 }
 
@@ -48,16 +38,16 @@ func TraceCtx(ctx context.Context, method string, format string, m ...interface{
 		}
 	}
 	startTime := time.Now()
-	std.Tprintf(LevelInfo, tag, "calling "+method+", "+format, m...)
+	std.Tprintf(Linfo, tag, "calling "+method+", "+format, m...)
 	return tag, method, startTime
 }
 
 // TraceOut 方法退出记录下消耗时间
 func TraceOut(tag string, method string, startTime time.Time) {
-	std.Tprintf(LevelInfo, tag, "finished "+method+", took %v", time.Since(startTime))
+	std.Tprintf(Linfo, tag, "finished "+method+", took %v", time.Since(startTime))
 }
 
-func Println(m ...interface{}) { std.Tprintf(LevelPrint, "", "", m...) }
+func Println(m ...interface{}) { std.Tprintf(Lprint, "", "", m...) }
 
 func getTracerIDFromCtx(ctx context.Context) string {
 	nid := "00000000-0000-0000-0000-000000000000"
@@ -75,73 +65,73 @@ func getTracerIDFromCtx(ctx context.Context) string {
 }
 
 func CtxDebugf(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelDebug, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Ldebug, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxDebug(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelDebug, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Ldebug, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxInfof(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelInfo, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Linfo, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxInfo(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelInfo, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Linfo, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxWarnf(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelWarn, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Lwarn, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxWarn(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelWarn, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lwarn, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxErrorf(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelError, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Lerror, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxError(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelError, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lerror, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxFatal(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelFatal, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lfatal, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxFatalf(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelFatal, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Lfatal, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxFatalln(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelFatal, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lfatal, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxPanic(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelPanic, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lpanic, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxPanicf(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelPanic, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Lpanic, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxPanicln(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelPanic, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lpanic, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxStack(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelStack, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lstack, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxPrint(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelPrint, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lprint, getTracerIDFromCtx(ctx), "", m...)
 }
 
 func CtxPrintf(ctx context.Context, format string, m ...interface{}) {
-	std.Tprintf(LevelPrint, getTracerIDFromCtx(ctx), format, m...)
+	std.Tprintf(Lprint, getTracerIDFromCtx(ctx), format, m...)
 }
 
 func CtxPrintln(ctx context.Context, m ...interface{}) {
-	std.Tprintf(LevelPrint, getTracerIDFromCtx(ctx), "", m...)
+	std.Tprintf(Lprint, getTracerIDFromCtx(ctx), "", m...)
 }
